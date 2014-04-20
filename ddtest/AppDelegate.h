952993e3e7bfee0d471510dc59db73650f7b06c5
corestrike <corestrike@gmail.com>
2014-04-20 16:11:07 +0900

    Initial Commit

---
+++ ddtest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ddtest
+//
+//  Created by corestrike on 2014/04/20.
+//  Copyright (c) 2014年 corestrike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
